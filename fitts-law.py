d29c3dd3a0d225659c96348f145e69b6b3545d3b
Karl Betcher <karl.betcher@mnsu.edu>
2024-01-18 16:26:42 -0600

    Added templates and made the landing page cooler

--- fitts-law.py
+++ fitts-law.py
@@ -1,13 +1,16 @@
 # import matplotlib.pyplot as plt
 # import numpy as np
 # import pandas as pd
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def landing_page():
-    return """<img src="https://cdn.discordapp.com/attachments/1019718536836427837/1197656100351262831/20240118_122620.jpg?ex=65bc0f29&is=65a99a29&hm=2799bebd6f096f500df19e47e5aee639bf02f3a7dd9d58c6fffb7373d85ecae1&" alt="the best boy">
-    <title>lmao Ethan is dumb</title>"""
+    return render_template("landing_page.html")
+
+@app.route("/informed-consent")
+def informed_consent():
+    return render_template("informed_consent.html")
 
 app.run(debug=True, port=5000)
