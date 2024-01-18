89b0172458711fee26b81928b95df777f64b546f
Karl Betcher <karl.betcher@mnsu.edu>
2024-01-18 16:53:44 -0600

    Updated some stuff (very descriptive I know)
    Merlin tracker- 3.75/10

--- fitts-law.py
+++ fitts-law.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def landing_page():
-    return render_template("landing_page.html")
+def home():
+    return render_template("index.html")
 
 @app.route("/informed-consent")
 def informed_consent():
