759e23957bcbb8ca84284bbec7f52da42dfb3af2
Madeline-Ellingson <93016306+Madeline-Ellingson@users.noreply.github.com>
2024-01-26 16:43:23 -0600

    Added loading page and start button

--- templates/main.js
+++ templates/main.js
@@ -35,7 +35,15 @@ function toggleElements(previousPageName, newPageName, loopSound, selectSound) {
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('pressA').addEventListener('click', function() {
-        toggleElements('healthAndSafetyPage', 'homePage', 'mainMenuSound', 'startSound');
+        document.querySelector('#healthAndSafetyPage').style.display = 'none';
+        document.querySelector('#loadingPage').style.display = 'block';
+        var audio = document.getElementById("startSound");
+        audio.play();
+    });
+
+    var wiiStartSound = document.getElementById('startSound');
+    wiiStartSound.addEventListener('ended', function() {
+    toggleElements('loadingPage', 'homePage', 'mainMenuSound', null);
     });
 
     document.getElementById('startButton').addEventListener('click', function() {
@@ -50,3 +58,42 @@ function playAudioAndRedirect(url) {
         window.location.href = url;
     }, audio.duration * 1000); // Redirect after the duration of the audio
 }
+
+// Animation for loading
+var squares = document.querySelectorAll('.loading-square');
+var indices = [0, 1, 2, 4, 7, 6, 5, 3];
+
+async function switchColor() {
+    for (let i = 0; i < indices.length; i++) {
+        if(squares[indices[i]].classList.contains('blue'))
+        {
+            squares[indices[i]].classList.remove('blue');
+            squares[indices[i]].classList.add('black');
+            await new Promise(resolve => setTimeout(resolve, 76));
+            squares[indices[i]].classList.remove('black');
+            squares[indices[i]].classList.add('blue');
+        }
+        else if(squares[indices[i]].classList.contains('medium-blue'))
+        {
+            squares[indices[i]].classList.remove('medium-blue');
+            squares[indices[i]].classList.add('black');
+            await new Promise(resolve => setTimeout(resolve, 76));
+            squares[indices[i]].classList.remove('black');
+            squares[indices[i]].classList.add('medium-blue');
+        }
+        else
+        {
+            squares[indices[i]].classList.remove('light-blue');
+            squares[indices[i]].classList.add('black');
+            await new Promise(resolve => setTimeout(resolve, 76));
+            squares[indices[i]].classList.remove('black');
+            squares[indices[i]].classList.add('light-blue');
+        }
+
+    }
+
+    setTimeout(switchColor, 0); // Call the function again after the sequence is completed
+}
+
+// Start the animation
+switchColor();
